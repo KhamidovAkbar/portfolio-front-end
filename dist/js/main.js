e2a9b371f10135a6ed18d59c85cc7a95ccfc7979
KhamidovAkbar <thesh3yx@gmail.com>
2019-05-07 02:57:36 +0900

    home page styles done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,19 @@
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu_btn');
-const menuBranding = document.querySelector('.menu__branding');
-const navbar = document.querySelector('.navbar');
-const navItems = document.querySelectorAll('.navbar__item');
+// FOR HAMBURGER MENU
+// const menu = document.querySelector('.menu');
+// const menuBtn = document.querySelector('.menu_btn');
+// const menuBranding = document.querySelector('.menu__branding');
+// const navbar = document.querySelector('.navbar');
+// const navItems = document.querySelectorAll('.navbar__item');
 
-let showMenu = false;
+// let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+// menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu() {
-	menuBtn.classList.toggle('close');
-	menu.classList.toggle('show');
-	menuBranding.classList.toggle('show');
-	navbar.classList.toggle('show');
-	navItems.forEach((item) => item.classList.toggle('show'));
-	showMenu = !showMenu;
-}
+// function toggleMenu() {
+// 	menuBtn.classList.toggle('close');
+// 	menu.classList.toggle('show');
+// 	menuBranding.classList.toggle('show');
+// 	navbar.classList.toggle('show');
+// 	navItems.forEach((item) => item.classList.toggle('show'));
+// 	showMenu = !showMenu;
+// }
